d06764b0a1cfa7c29141ac5b3cef79e2971b88d4
ZioCessoProgramma <benjaminval@outlook.it>
2026-04-18 20:35:38 +0200

    Framebuffer class added

---
+++ Trinacria/include/Trinacria/FrameBuffer.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+#include <cstdint>
+
+#include "Macros.h"
+
+namespace TRCN_CORE_NAMESPACE
+{
+    class FrameBuffer
+    {
+    public:
+        FrameBuffer();
+        ~FrameBuffer();
+
+        void Bind();
+
+        void AttachTexture(uint32_t attachment, uint32_t textureType, uint32_t textureId);
+
+    private:
+        uint32_t _frameBufferId;
+    };
+}
---
+++ Trinacria/src/FrameBuffer.cpp
@@ -0,0 +1,24 @@
+﻿#include "Trinacria/FrameBuffer.h"
+#include <glad/glad.h>
+
+Trinacria::DSL::FrameBuffer::FrameBuffer()
+{
+    glGenFramebuffers(1, &_frameBufferId);
+
+    Bind();
+}
+
+Trinacria::DSL::FrameBuffer::~FrameBuffer()
+{
+    glDeleteFramebuffers(1, &_frameBufferId);
+}
+
+void Trinacria::DSL::FrameBuffer::Bind()
+{
+    glBindFramebuffer(GL_FRAMEBUFFER, _frameBufferId);
+}
+
+void Trinacria::DSL::FrameBuffer::AttachTexture(uint32_t attachment, uint32_t textureType, uint32_t textureId)
+{
+    glFramebufferTexture2D(GL_FRAMEBUFFER, attachment, textureType, textureId, 0);
+}
