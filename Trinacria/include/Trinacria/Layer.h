85d882a5a9c2879c89e85fa8bd8ecc6963c0bd7f
ZioCessoProgramma <benjaminval@outlook.it>
2026-08-11 23:22:59 +0200

    added documentation to Layer.h

--- Trinacria/include/Trinacria/Layer.h
+++ Trinacria/include/Trinacria/Layer.h
@@ -7,9 +7,26 @@ namespace TRCN_CORE_NAMESPACE
 	TRCN_ABSTRACT class Layer
 	{
 	public:
+		/**
+		 * @brief function to call every frame
+		 * @note should be called every frame
+		 * @param deltaTime the time that passes between frame in seconds
+		 */
+		
 		virtual void OnUpdate(float deltaTime) = 0;
 
+		/**
+		 * @brief should be called when the layer is attached
+		 * @note  should be called when the layer is attached
+		 */
+
 		virtual void OnAttach() = 0;
+
+		/**
+		 * @brief should be called when the layer is attached
+		 * @note  should be called when the layer is attached
+		 */
+
 		virtual void OnDetach() = 0;
 
 		virtual ~Layer() = default;
